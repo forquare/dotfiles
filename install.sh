bc12168e9471226e80f013faf826a3885678d61b
Ben Lavery <ben.lavery@hashbang0.com>
2014-11-25 12:01:06 +0000

    Fixed install.sh to work with recent file changes

--- install.sh
+++ install.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
-# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 DIR=~/Dropbox/WorkBench/dotfiles
 echo DIR
-ror file in  bashrc  gitignore
-do
-  if [ -e $HOME/.${file} ]
-  then echo ".$file found, doing nothing"
-  else ln -sf $DIR/$file $HOME/.$file && echo ".$file installed"
-  fi
+for file in bashrc gitignore; do
+	if [ -e $HOME/.${file} ]
+	then echo ".$file found, doing nothing"
+	else ln -sf $DIR/$file $HOME/.$file && echo ".$file installed"
+	fi
 done
 
 # gitconfig
 if [ -e $HOME/.gitconfig ]
 then echo ".gitconfig found, doing nothing"
 else 
-  echo "Git Details"
-  echo "-----------"
-  read -p "Name " name
-  read -p "Email " email
-  read -p "Github username " github
+	echo "Git Details"
+	echo "-----------"
+	read -p "Name " name
+	read -p "Email " email
+	read -p "Github username " github
 
-  # escape strings for sed
-  name=$(printf "%s\n" "$name" | sed 's/[\&/]/\\&/g')
-  email=$(printf "%s\n" "$email" | sed 's/[\&/]/\\&/g')
-  github=$(printf "%s\n" "$github" | sed 's/[\&/]/\\&/g')
-  home=$(printf "%s\n" "$HOME" | sed 's/[\&/]/\\&/g')
+	# escape strings for sed
+	name=$(printf "%s\n" "$name" | sed 's/[\&/]/\\&/g')
+	email=$(printf "%s\n" "$email" | sed 's/[\&/]/\\&/g')
+	github=$(printf "%s\n" "$github" | sed 's/[\&/]/\\&/g')
+	home=$(printf "%s\n" "$HOME" | sed 's/[\&/]/\\&/g')
 
-  # replace NAME, EMAIL, GITHUB, HOME
-  sed "s/NAME/$name/g" $DIR/gitconfig | sed "s/EMAIL/$email/g" | sed "s/GITHUB/$github/g" | sed "s/HOME/$home/g" > $HOME/.gitconfig
-  echo ".gitconfig installed"
+	# replace NAME, EMAIL, GITHUB, HOME
+	sed "s/NAME/$name/g" $DIR/gitconfig | sed "s/EMAIL/$email/g" | sed "s/GITHUB/$github/g" | sed "s/HOME/$home/g" > $HOME/.gitconfig
+	echo ".gitconfig installed"
 fi
