bca378d5d213d3741be7c0b1c7d88ecfab6b6d7e
Ben Lavery <ben.lavery@hashbang0.com>
2019-01-15 22:26:50 +0000

    Add conky

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ for file in oh-my-tmux/.tmux.conf ; do
 done
 
 if [ $(uname) == "FreeBSD" ]; then
-	for file in xsession xscreensaver conkyrc cwmrc Xresources; do
+	for file in xsession xscreensaver conky.d cwmrc Xresources; do
 		if [ ! -e $HOME/.${file} ]; then
 			ln -sf $DIR/$file $HOME/.$file && echo ".$file installed"
 			CHANGED=1
